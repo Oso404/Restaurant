f78f978d6fc84223ade9497ccbfd0fc2a8b6f328
Oso404 <joules.programming.8122@gmail.com>
2025-07-22 20:51:47 -0400

    fixing up registration page

--- src/Starter.jsx
+++ src/Starter.jsx
@@ -1,4 +1,5 @@
-import './Starter.css'
+import './style/Starter.css'
+import logo from "./assets/couple2.png"
 
 let isSignedIn = false;
 
@@ -7,10 +8,15 @@ function StarterCard(){
         <div className = "outer-body">
             <div className = "outer-card">
                 <div className = "inner-card-left">
-                    <p>dd</p>
+                    <div className = "logo-wrapper">
+                        <p id = "logo-top-text">Welcome Back</p>
+                        <p id = "logo-bottom-text">Sign in to access your personalized homepage, follow restaurants
+                            and foods you love, and clap for stories that matter to you.</p>
+                        <img src={logo} alt='logo'/>
+                    </div>
                 </div>
                 <div className = "inner-card-right">
-                    <p>ee</p>
+                    <p>Right Card</p>
                 </div>
             </div>
 
